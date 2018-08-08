84a6dfe0675d9bd811f66b964f6c98aeeb09cbfe
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-08-08 11:53:00 -0400

    fixes https url issue

--- backup_db.sh
+++ backup_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 BACKUP_DATE=$(date +%Y-%m-%d"-"%H%M%S);
-pg_dump -Fc -U joshua -h localhost opc-api_development -f ./db/db_backup_$BACKUP_DATE.psql
+pg_dump -Fc --no-acl --no-owner -h localhost -U joshua opc-api_development > ./db/db_backup_$BACKUP_DATE.psql
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+Rails.application.routes.default_url_options[:protocol] = 'https'
 Rails.application.routes.default_url_options[:host] = 'opc-vehicledb-api.herokuapp.com'
