d652b1777b255c95bddcd2ec987aeb9831bb4ef1
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-08-08 10:54:41 -0400

    Fixes host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,3 +83,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+Rails.application.routes.default_url_options[:host] = 'opc-vehicledb-api.herokuapp.com'
