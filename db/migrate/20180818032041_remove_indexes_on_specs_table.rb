d0c503748a4a7fd94ed2e7932f2fc22948535b73
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-08-23 13:56:25 -0400

    fixes more migrations

--- db/migrate/20180818032041_remove_indexes_on_specs_table.rb
+++ db/migrate/20180818032041_remove_indexes_on_specs_table.rb
@@ -1,9 +1,9 @@
 class RemoveIndexesOnSpecsTable < ActiveRecord::Migration[5.2]
   def change
-    remove_index :vehicle_specs, column: :vehicle_make_id, name: :index_vehicle_specs_on_vehicle_make_id
-    remove_index :vehicle_specs, column: :vehicle_make_id, name: :index_vehicle_specs_on_vehicle_model_id
+    # remove_index :vehicle_specs, column: :vehicle_make_id, name: :index_vehicle_specs_on_vehicle_make_id
+    # remove_index :vehicle_specs, column: :vehicle_make_id, name: :index_vehicle_specs_on_vehicle_model_id
 
-    remove_reference :vehicle_specs, :vehicle_make
-    remove_reference :vehicle_specs, :vehicle_model
+    # remove_reference :vehicle_specs, :vehicle_make
+    # remove_reference :vehicle_specs, :vehicle_model
   end
 end
