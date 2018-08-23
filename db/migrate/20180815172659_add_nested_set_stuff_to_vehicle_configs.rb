871860594cb8a100eec080708e07783643665ec3
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-08-23 13:53:00 -0400

    fixes migration

--- db/migrate/20180815172659_add_nested_set_stuff_to_vehicle_configs.rb
+++ db/migrate/20180815172659_add_nested_set_stuff_to_vehicle_configs.rb
@@ -1,6 +1,6 @@
 class AddNestedSetStuffToVehicleConfigs < ActiveRecord::Migration[5.2]
   def self.up
-    add_column :vehicle_configs, :parent_id, :integer # Comment this line if your project already has this column
+    # add_column :vehicle_configs, :parent_id, :integer # Comment this line if your project already has this column
     # VehicleConfig.where(parent_id: 0).update_all(parent_id: nil) # Uncomment this line if your project already has :parent_id
     add_column :vehicle_configs, :lft,       :integer
     add_column :vehicle_configs, :rgt,       :integer
