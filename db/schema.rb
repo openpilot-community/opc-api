d84c004ccf160971bb584184bcbada351ec1dee6
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-09-03 19:08:22 -0400

    updates db for guides

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_045947) do
+ActiveRecord::Schema.define(version: 2018_09_03_230734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2018_09_03_045947) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "source_image_url"
+    t.string "article_source_url"
     t.index ["user_id"], name: "index_guides_on_user_id"
   end
 
